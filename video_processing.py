5ab1b77260efdbdc7d2380011c8fd4d1c0aef240
Brendon Ky <brendonky18@users.noreply.github.com>
2021-12-09 05:22:29 -0500

    implemented tracking code

--- video_processing.py
+++ video_processing.py
@@ -2,26 +2,50 @@ import cv2
 import numpy as np
 
 from imutils.object_detection import non_max_suppression as nms
+from time import time_ns as nanoseconds
 
 FRONT_FACE_CASCADE = "haar_cascades/haarcascade_frontalface_default.xml"
 PROFILE_FACE_CASCADE = "haar_cascades/haarcascade_profileface.xml"
+DETECTION_PERIOD = 500
 
-def cascade_detect(image, cascade):
+def millis():
+    return nanoseconds() // 1000000
+
+def detect(image, detector):
     grayscale_frame = cv2.equalizeHist(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
 
-    detected_objects = cascade.detectMultiScale(grayscale_frame)
+    results = detector.detectMultiScale(grayscale_frame)
 
-    return [(x_coord, y_coord, x_coord+width, y_coord+height) for (x_coord, y_coord, width, height) in detected_objects]
+    detected_objects = results
+    
+    if isinstance(detector, cv2.HOGDescriptor):      # HOG
+        # print("H", flush=True)
+        # print(results, flush=True)
+        detected_objects = results[0]
+    elif isinstance(detector, cv2.CascadeClassifier):                                           # Cascade
+        detected_objects = results
+    else:
+        raise RuntimeError("Can only handle detectors that are HOGs or Haar Cascades")
+
+    # print(detected_objects)
+    return nms(np.array([(x_coord, y_coord, x_coord+width, y_coord+height) for (x_coord, y_coord, width, height) in detected_objects]), overlapThresh=0.7)
+    # return [(x_coord, y_coord, x_coord+width, y_coord+height) for (x_coord, y_coord, width, height) in detected_objects]
 
     # for (x_coord, y_coord, width, height) in detected_objects:
 
     # pass
 
-def render_frame(frame):
+def render_frame(frame, bounding_boxes):
+    # print("H")
+    # print(bounding_boxes)
+    for (xA, yA, xB, yB) in bounding_boxes:
+        # print("h")
+        # print((xA, yA))
+        # print((xB, yB))
+        # cv2.rectangle(frame, (0, 0), (10, 10), (0,0,255))
+        cv2.rectangle(frame, (xA, yA), (xB, yB), (0,0,255))
 
     cv2.imshow("Frame", frame)
-
-
 
 def main():
     #  init cascades
@@ -36,41 +60,78 @@ def main():
 
     # init Histogram of Oriented Gradients (HOG)
     HOG_detector = cv2.HOGDescriptor()
-    HOG_detector.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    HOG_detector.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector(), )
 
-    # get the camera feed
+    # init detectors
+    detectors = [front_face_cascade, profile_face_cascade, HOG_detector]
+    next_detector = 0
+
+    # init tracker
+    multiTrackers = [cv2.legacy.MultiTracker_create() for i in detectors]
+
+    # init time
+    cur_time = millis()
+    detection_time = cur_time - 1
+
+    # init the camera feed
     camera_feed = cv2.VideoCapture(1)
 
     # output to the screen
     while(camera_feed.isOpened()) :
         is_capturing_feed, cur_frame = camera_feed.read()
-        if is_capturing_feed:
-            # front_faces = cascade_detect(cur_frame, front_face_cascade)
-            profile_faces = cascade_detect(cur_frame, profile_face_cascade)
-
-            people, weights = HOG_detector.detectMultiScale(cur_frame)
-
-            people = nms(np.array([(x_coord, y_coord, x_coord+width, y_coord+height) for (x_coord, y_coord, width, height) in people]), overlapThresh=0.7)
-
-            drawn_frame = cur_frame
-
-            # print(type(profile_faces))
-            # print(type(people))
-
-            for (xA, yA, xB, yB) in profile_faces:
-                drawn_frame = cv2.rectangle(drawn_frame, (xA, yA), (xB, yB), (0,0,255))
-
-            for (xA, yA, xB, yB) in people:
-                drawn_frame = cv2.rectangle(drawn_frame, (xA, yA), (xB, yB), (0,255,0))
-
-            render_frame(drawn_frame)
-
-            # press q to close the program
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-
-        else:
+        if not is_capturing_feed:
             break
+        
+        # press q to close the program
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+        
+        # distribute each of the dectors over the entire period 
+        # so we're not performing several large calculations simultaneously
+        # print(f"cur time: {cur_time}")
+        # print(f"dtc time: {detection_time}")
+        if cur_time >= (detection_time + DETECTION_PERIOD/len(detectors)):
+            # print("Detecting")
+            detected_objects = detect(cur_frame, detectors[next_detector])
+            
+            # get rid of old trackers so we don't have duplicates, recreate tracker
+            multiTrackers[next_detector] = cv2.legacy.MultiTracker_create()
+            
+            # assign objects to tracker
+            for object in detected_objects:
+                multiTrackers[next_detector].add(cv2.legacy.TrackerKCF_create(), cur_frame, object)
+
+            detection_time = int(DETECTION_PERIOD/len(detectors) + cur_time)
+            next_detector = (next_detector + 1) % len(detectors)
+        # else:
+            # print("Tracking")
+
+        cur_time = millis()
+
+        # update trackers
+        tracked_objects = []
+        for (successes, cur_tracked_objects) in [multiTracker.update(cur_frame) for multiTracker in multiTrackers]:
+            tracked_objects += [(int(x_coord), int(y_coord), int(x_coord+width), int(y_coord+height)) for (x_coord, y_coord, width, height) in cur_tracked_objects]
+
+        # front_faces = detect(cur_frame, front_face_cascade)
+        # profile_faces = detect(cur_frame, profile_face_cascade)
+
+        # people, weights = HOG_detector.detectMultiScale(cur_frame, winStride=(4, 4), scale=1.15)
+
+        # people = 
+
+        # drawn_frame = cur_frame
+
+        # print(type(profile_faces))
+        # print(type(people))
+
+        # for (xA, yA, xB, yB) in profile_faces:
+        #     drawn_frame = cv2.rectangle(drawn_frame, (xA, yA), (xB, yB), (0,0,255))
+
+        # for (xA, yA, xB, yB) in people:
+        #     drawn_frame = cv2.rectangle(drawn_frame, (xA, yA), (xB, yB), (0,255,0))
+
+        render_frame(cur_frame, tracked_objects)
 
     # clean up
     camera_feed.release()
